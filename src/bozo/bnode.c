67ecc4280a1d7738b1294350e4f64a53579f65f3
Chas Williams (CONTRACTOR) <chas@cmf.nrl.navy.mil>
2012-11-30 16:06:08 -0500

    bozo: initial pthread support
    Add pthread signal handling to bosserver to using softsig.  IOMGR_Sleep()
    simply converted to sleep() for bosserver and bos.
    
    Switch to using the new spawnprocve_sig() to mask all signals except for
    SIGTERM, SIGKILL, and SIGQUIT in the child process, to avoid issues
    with per-thread signal masks.
    
    Change-Id: I4bf741e04a6f87a1551dd831a20851b56b10d910

--- src/bozo/bnode.c
+++ src/bozo/bnode.c
@@ -20,7 +20,11 @@
 #include <afs/audit.h>
 #include <afs/afsutil.h>
 #include <afs/fileutil.h>
+#include <opr/lock.h>
 #include <opr/queue.h>
+#ifndef AFS_NT40_ENV
+#include <afs/softsig.h>
+#endif
 
 #include "bnode.h"
 #include "bnode_internal.h"
@@ -34,7 +38,13 @@
 #define BNODE_ERROR_COUNT_MAX   16   /* maximum number of retries */
 #define BNODE_ERROR_DELAY_MAX   60   /* maximum retry delay (seconds) */
 
-static PROCESS bproc_pid;	/* pid of waker-upper */
+#ifdef AFS_PTHREAD_ENV
+static pthread_t bproc_pid;
+static opr_cv_t bproc_cv;
+static opr_mutex_t bproc_mutex;
+#else
+static PROCESS bproc_pid;	/**< pid of waker-upper */
+#endif
 static struct opr_queue allBnodes;	/**< List of all bnodes */
 static struct opr_queue allProcs;	/**< List of all processes for which we're waiting */
 static struct opr_queue allTypes;	/**< List of all registered type handlers */
@@ -483,7 +493,11 @@ bnode_SetTimeout(struct bnode *abnode, afs_int32 atimeout)
 	abnode->nextTimeout = FT_ApproxTime() + atimeout;
 	abnode->flags |= BNODE_NEEDTIMEOUT;
 	abnode->period = atimeout;
+#ifdef AFS_PTHREAD_ENV
+	opr_cv_signal(&bproc_cv);
+#else
 	IOMGR_Cancel(bproc_pid);
+#endif
     } else {
 	abnode->flags &= ~BNODE_NEEDTIMEOUT;
     }
@@ -526,6 +540,10 @@ bproc(void *unused)
     int setAny;
     int status;
 
+#ifdef AFS_PTHREAD_ENV
+    opr_mutex_enter(&bproc_mutex);
+#endif
+
     while (1) {
 	/* first figure out how long to sleep for */
 	temp = 0x7fffffff;	/* afs_int32 time; maxint doesn't work in select */
@@ -547,9 +565,20 @@ bproc(void *unused)
 	else
 	    temp = 999999;
 	if (temp > 0) {
+#ifdef AFS_PTHREAD_ENV
+	    struct timespec ts;
+
+	    ts.tv_sec = time(NULL) + temp;
+	    ts.tv_nsec = 0;
+	    if (opr_cv_timedwait(&bproc_cv, &bproc_mutex, &ts) == 0)
+		code = -1;      /* wait was cancelled */
+	    else
+		code = 0;       /* wait timed out */
+#else
 	    tv.tv_sec = temp;
 	    tv.tv_usec = 0;
 	    code = IOMGR_Select(0, 0, 0, 0, &tv);
+#endif
 	} else
 	    code = 0;		/* fake timeout code */
 
@@ -807,7 +836,11 @@ bnode_SoftInt(void *param)
 {
     /* int asignal = (int) param; */
 
+#ifdef AFS_PTHREAD_ENV
+    opr_cv_signal(&bproc_cv);
+#else
     IOMGR_Cancel(bproc_pid);
+#endif
     return NULL;
 }
 
@@ -817,11 +850,17 @@ bnode_SoftInt(void *param)
 void
 bnode_Int(int asignal)
 {
-    if (asignal == SIGQUIT || asignal == SIGTERM) {
+#ifdef AFS_PTHREAD_ENV
+    if (asignal == SIGQUIT || asignal == SIGTERM)
+	bozo_ShutdownAndExit((void *)(intptr_t) asignal);
+    else
+	bnode_SoftInt((void *)(intptr_t) asignal);
+#else
+    if (asignal == SIGQUIT || asignal == SIGTERM)
 	IOMGR_SoftSig(bozo_ShutdownAndExit, (void *)(intptr_t)asignal);
-    } else {
+    else
 	IOMGR_SoftSig(bnode_SoftInt, (void *)(intptr_t)asignal);
-    }
+#endif
 }
 
 
@@ -829,9 +868,16 @@ bnode_Int(int asignal)
 int
 bnode_Init(void)
 {
+#ifdef AFS_PTHREAD_ENV
+    AFS_SIGSET_DECL;
+    pthread_attr_t tattr;
+#else
     PROCESS junk;
+#endif
     afs_int32 code;
+#if !defined(AFS_PTHREAD_ENV) || defined(AFS_NT40_ENV)
     struct sigaction newaction;
+#endif
     static int initDone = 0;
 
     if (initDone)
@@ -841,13 +887,29 @@ bnode_Init(void)
     opr_queue_Init(&allProcs);
     opr_queue_Init(&allBnodes);
     memset(&bnode_stats, 0, sizeof(bnode_stats));
+#ifdef AFS_PTHREAD_ENV
+    opr_cv_init(&bproc_cv);
+    opr_mutex_init(&bproc_mutex);
+
+    pthread_attr_init(&tattr);
+    pthread_attr_setdetachstate(&tattr, PTHREAD_CREATE_DETACHED);
+    AFS_SIGSET_CLEAR();
+    code = pthread_create(&bproc_pid, &tattr, bproc, NULL);
+    AFS_SIGSET_RESTORE();
+#else
     LWP_InitializeProcessSupport(1, &junk);	/* just in case */
     IOMGR_Initialize();
     code = LWP_CreateProcess(bproc, BNODE_LWP_STACKSIZE,
 			     /* priority */ 1, (void *) /* parm */ 0,
 			     "bnode-manager", &bproc_pid);
+#endif
     if (code)
 	return code;
+#if defined(AFS_PTHREAD_ENV) && !defined(AFS_NT40_ENV)
+    softsig_signal(SIGCHLD, bnode_Int);
+    softsig_signal(SIGQUIT, bnode_Int);
+    softsig_signal(SIGTERM, bnode_Int);
+#else
     memset(&newaction, 0, sizeof(newaction));
     newaction.sa_handler = bnode_Int;
     code = sigaction(SIGCHLD, &newaction, NULL);
@@ -859,6 +921,7 @@ bnode_Init(void)
     code = sigaction(SIGTERM, &newaction, NULL);
     if (code)
 	return errno;
+#endif
     return code;
 }
 
@@ -945,6 +1008,7 @@ bnode_NewProc(struct bnode *abnode, char *aexecString, char *coreName,
     pid_t cpid;
     char *argv[MAXVARGS];
     int i;
+    sigset_t set;
 
     code = bnode_ParseLine(aexecString, &tlist);	/* try parsing first */
     if (code)
@@ -963,7 +1027,11 @@ bnode_NewProc(struct bnode *abnode, char *aexecString, char *coreName,
     }
     argv[i] = NULL;		/* null-terminated */
 
-    cpid = spawnprocve(argv[0], argv, environ, -1);
+    sigfillset(&set);
+    sigdelset(&set, SIGKILL);
+    sigdelset(&set, SIGTERM);
+    sigdelset(&set, SIGQUIT);
+    cpid = spawnprocve_sig(argv[0], argv, environ, -1, &set);
     osi_audit(BOSSpawnProcEvent, 0, AUD_STR, aexecString, AUD_END);
 
     if (cpid == (pid_t) - 1) {
