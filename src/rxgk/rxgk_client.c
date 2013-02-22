268028db94768e5d25d670519df6217334ca229a
Ben Kaduk <kaduk@mit.edu>
2013-02-22 18:35:42 -0500

    Implement GetResponse

--- src/rxgk/rxgk_client.c
+++ src/rxgk/rxgk_client.c
@@ -46,6 +46,12 @@
 #include <rx/rxgk.h>
 
 #include "rxgk_private.h"
+#include "../rx/rx_conn.h"
+#include "../rx/rx_call.h"
+
+/* This prototype is in afs_prototypes.h, which we can't include here.
+ * We will build this object and link it directly. */
+afs_int32 afs_uuid_create(afsUUID * uuid);
 
 static struct rx_securityOps rxgk_client_ops = {
     rxgk_Close,
@@ -99,11 +105,216 @@ rxgk_NewClientSecurityObject(RXGK_Level level, afs_int32 enctype, rxgk_key k0,
     return sc;
 }
 
-/* Respondn to a challenge packet */
+/*
+ * Helpers for GetResponse.
+ */
+static int
+fill_authenticator(RXGK_Authenticator *authenticator, char *nonce,
+		   struct rxgk_cprivate *cp, struct rx_connection *aconn)
+{
+    XDR xdrs;
+    afsUUID uuid;
+    afs_uint32 call_numbers[RX_MAXCALLS], maxcall;
+    int ret, i, ncalls;
+    u_int len;
+
+    memset(&xdrs, 0, sizeof(xdrs));
+
+    memcpy(authenticator->nonce, nonce, 20);
+    /* Must encode the uuid manually. */
+    afs_uuid_create(&uuid);
+    xdrlen_create(&xdrs);
+    if (!xdr_afsUUID(&xdrs, &uuid)) {
+	ret = RXGEN_CC_MARSHAL;
+	goto cleanup;
+    }
+    len = xdr_getpos(&xdrs);
+    authenticator->appdata.val = xdr_alloc(len);
+    if (authenticator->appdata.val == NULL) {
+	ret = RXGEN_CC_MARSHAL;
+	goto cleanup;
+    }
+    xdr_destroy(&xdrs);
+    xdrmem_create(&xdrs, authenticator->appdata.val, len, XDR_ENCODE);
+    if (!xdr_afsUUID(&xdrs, &uuid)) {
+	ret = RXGEN_CC_MARSHAL;
+	goto cleanup;
+    }
+
+    authenticator->level = cp->level;
+    /* XXX need a better story... */
+    authenticator->epoch = aconn->epoch;
+    /* XXX */
+    authenticator->cid = aconn->cid;
+    /* Grunge about in the calls */
+    ncalls = maxcall = 0;
+    for(i = 0; i < RX_MAXCALLS; ++i) {
+	if (aconn->call[i] != NULL &&
+	    (aconn->call[i]->state == RX_STATE_ACTIVE ||
+	    aconn->call[i]->state == RX_STATE_PRECALL)) {
+	    call_numbers[ncalls++] = aconn->callNumber[i];
+	    maxcall = (maxcall > aconn->callNumber[i]) ? maxcall :
+							 aconn->callNumber[i];
+	}
+    }
+    authenticator->maxcalls = maxcall;
+    authenticator->call_numbers.len = ncalls;
+    authenticator->call_numbers.val = xdr_alloc(ncalls * sizeof(afs_uint32));
+    if (authenticator->call_numbers.val == NULL) {
+	ret = RXGEN_CC_MARSHAL;
+	goto cleanup;
+    }
+    for(i = 0; i < ncalls; ++i)
+	authenticator->call_numbers.val[i] = call_numbers[i];
+    ret = 0;
+cleanup:
+    xdr_destroy(&xdrs);
+    return ret;
+}
+
+/* XDR-encode an authenticator and encrypt it. */
+static int
+pack_wrap_authenticator(RXGK_Data *encdata, RXGK_Authenticator *authenticator,
+			struct rxgk_cprivate *cp)
+{
+    XDR xdrs;
+    RXGK_Data data;
+    int ret;
+    u_int len;
+
+    memset(&xdrs, 0, sizeof(xdrs));
+    zero_rxgkdata(&data);
+
+    xdrlen_create(&xdrs);
+    if (!xdr_RXGK_Authenticator(&xdrs, authenticator)) {
+	ret = RXGEN_CC_MARSHAL;
+	goto cleanup;
+    }
+    len = xdr_getpos(&xdrs);
+    data.val = xdr_alloc(len);
+    if (data.val == NULL) {
+	ret = RXGEN_CC_MARSHAL;
+	goto cleanup;
+    }
+    data.len = len;
+    xdr_destroy(&xdrs);
+    xdrmem_create(&xdrs, data.val, len, XDR_ENCODE);
+    if (!xdr_RXGK_Authenticator(&xdrs, authenticator)) {
+	ret = RXGEN_CC_MARSHAL;
+	goto cleanup;
+    }
+    ret = encrypt_in_key(cp->k0, RXGK_CLIENT_ENC_RESPONSE, &data, encdata);
+    if (ret != 0)
+	goto cleanup;
+
+cleanup:
+    xdr_destroy(&xdrs);
+    xdr_free((xdrproc_t)xdr_RXGK_Data, &data);
+    return ret;
+}
+
+/* XDR-encode an RXGK_Response structure to put it on the wire.
+ * The caller must free the out parameter. */
+static int
+pack_response(RXGK_Data *out, RXGK_Response *response)
+{
+    XDR xdrs;
+    int ret;
+    u_int len;
+
+    memset(&xdrs, 0, sizeof(xdrs));
+
+    xdrlen_create(&xdrs);
+    if (!xdr_RXGK_Response(&xdrs, response)) {
+	ret = RXGEN_CC_MARSHAL;
+	goto cleanup;
+    }
+    len = xdr_getpos(&xdrs);
+    out->val = xdr_alloc(len);
+    if (out->val == NULL) {
+	ret = RXGEN_CC_MARSHAL;
+	goto cleanup;
+    }
+    out->len = len;
+    xdr_destroy(&xdrs);
+    xdrmem_create(&xdrs, out->val, len, XDR_ENCODE);
+    if (!xdr_RXGK_Response(&xdrs, response)) {
+	ret = RXGEN_CC_MARSHAL;
+	goto cleanup;
+    }
+    ret = 0;
+
+cleanup:
+    xdr_destroy(&xdrs);
+    return ret;
+}
+
+/*
+ * Respond to a challenge packet.
+ * The data of the packet on entry is the XDR-encoded RXGK_Challenge.
+ * We decode it and reuse the packet structure to prepare a response.
+ */
 int
 rxgk_GetResponse(struct rx_securityClass *aobj, struct rx_connection *aconn,
 		 struct rx_packet *apacket)
 {
-    /* XXXBJK */
-    return 0;
+    struct rxgk_cprivate *cp;
+    struct rxgk_cconn *cc;
+    XDR xdrs;
+    RXGK_Challenge challenge;
+    RXGK_Response response;
+    RXGK_Authenticator authenticator;
+    RXGK_Data encdata, packed;
+    int ret;
+
+    memset(&xdrs, 0, sizeof(xdrs));
+    memset(&challenge, 0, sizeof(challenge));
+    memset(&response, 0, sizeof(response));
+    memset(&authenticator, 0, sizeof(authenticator));
+    zero_rxgkdata(&encdata);
+    zero_rxgkdata(&packed);
+
+    cp = aobj->privateData;
+    cc = rx_GetSecurityData(aconn);
+
+    /* Decode the challenge to get the nonce. */
+    xdrmem_create(&xdrs, rx_DataOf(apacket), rx_GetDataSize(apacket),
+		  XDR_DECODE);
+    if (!xdr_RXGK_Challenge(&xdrs, &challenge)) {
+	ret = RXGEN_CC_UNMARSHAL;
+	goto cleanup;
+    }
+
+    /* Start filling the response. */
+    response.start_time = cc->start_time;
+    if (copy_rxgkdata(&response.token, &cp->token) != 0) {
+	ret = RXGEN_CC_MARSHAL;
+	goto cleanup;
+    }
+
+    /* Fill up the authenticator */
+    ret = fill_authenticator(&authenticator, challenge.nonce, cp, aconn);
+    if (ret != 0)
+	goto cleanup;
+    /* Authenticator is full, now to pack and encrypt it. */
+    ret = pack_wrap_authenticator(&encdata, &authenticator, cp);
+    if (ret != 0)
+	goto cleanup;
+    rx_opaque_populate(&response.authenticator, encdata.val, encdata.len);
+
+    /* Response is ready, now to shove it in a packet. */
+    ret = pack_response(&packed, &response);
+    if (ret != 0)
+	goto cleanup;
+    rx_packetwrite(apacket, 0, packed.len, packed.val);
+    rx_SetDataSize(apacket, packed.len);
+
+cleanup:
+    xdr_destroy(&xdrs);
+    xdr_free((xdrproc_t)xdr_RXGK_Challenge, &challenge);
+    xdr_free((xdrproc_t)xdr_RXGK_Response, &response);
+    xdr_free((xdrproc_t)xdr_RXGK_Authenticator, &authenticator);
+    xdr_free((xdrproc_t)xdr_RXGK_Data, &encdata);
+    xdr_free((xdrproc_t)xdr_RXGK_Data, &packed);
+    return ret;
 }
