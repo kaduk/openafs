83fdf27724e02a73f1651be793220a1cfa30e371
Ben Kaduk <kaduk@mit.edu>
2013-03-27 17:02:55 -0400

    Export fortuna PRNG to the kernel where needed
    Some systems do not supply a useful implementation of osi_readRandom()
    but we want to use real random numbers in the kernel for setting the
    RX epoch and connection ID.
    
    Build hcrypto's fortuna PRNG into the rand-kernel interface we expose,
    and export RAND_seed so that we can pass in entropy from userland.
    
    If we are going to use the fortuna implementation, we must include the
    fortuna implementation in the objects linked into the kernel module; do
    this only on the architectures where it is needed.
    
    Change-Id: I614d2bd9ac52803ec3b9572cc694cd836c8427dd

--- src/crypto/hcrypto/kernel/rand.c
+++ src/crypto/hcrypto/kernel/rand.c
@@ -7,12 +7,29 @@
 #include <aes.h>
 #include <sha.h>
 
+void
+RAND_seed(const void *indata, size_t size)
+{
+#if defined(AFS_AIX_ENV) || defined(AFS_DFBSD_ENV) || defined(AFS_HPUX_ENV) || defined(AFS_SGI_ENV) || defined(UKERNEL)
+    RAND_METHOD *m = RAND_fortuna_method();
+    m->seed(indata, size);
+#else
+    /* Do nothing; we use the kernel's RNG */
+    return;
+#endif
+}
+
 int
 RAND_bytes(void *outdata, size_t size)
 {
     if (size == 0)
 	return 0;
+#if defined(AFS_AIX_ENV) || defined(AFS_DFBSD_ENV) || defined(AFS_HPUX_ENV) || defined(AFS_SGI_ENV) || defined(UKERNEL)
+    RAND_METHOD *m = RAND_fortuna_method();
+    return m->bytes(outdata, size);
+#else
     if (osi_readRandom(outdata, size))
 	return 0;
+#endif
     return 1;
 }
