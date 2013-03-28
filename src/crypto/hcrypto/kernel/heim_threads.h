0c8de3a7fc85e7c72cff80d9053251f059355d7c
Ben Kaduk <kaduk@mit.edu>
2013-03-28 16:54:09 -0400

    Fixup HEIMDAL_MUTEX macros, sigh

--- src/crypto/hcrypto/kernel/heim_threads.h
+++ src/crypto/hcrypto/kernel/heim_threads.h
@@ -1,7 +1,8 @@
+#include <rx_kmutex.h>
 #define HEIMDAL_MUTEX afs_kmutex_t*
 extern afs_kmutex_t hckernel_mutex;
 #define HEIMDAL_MUTEX_INITIALIZER	&hckernel_mutex;
 #define HEIMDAL_MUTEX_init(m)		MUTEX_INIT(*m,0,0,0)
-#define HEIMDAL_MUTEX_lock(m)		MUTEX_LOCK(*m)
-#define HEIMDAL_MUTEX_unlock(m)		MUTEX_UNLOCK(*m)
+#define HEIMDAL_MUTEX_lock(m)		MUTEX_ENTER(*m)
+#define HEIMDAL_MUTEX_unlock(m)		MUTEX_EXIT(*m)
 #define HEIMDAL_MUTEX_destroy(m)	MUTEX_DESTROY(*m)
