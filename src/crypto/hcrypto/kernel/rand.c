d231932471ae0d9a67de8f17f5e9406d132e887a
Ben Kaduk <kaduk@mit.edu>
2013-03-28 13:59:37 -0400

    Sprinkle const

--- src/crypto/hcrypto/kernel/rand.c
+++ src/crypto/hcrypto/kernel/rand.c
@@ -7,11 +7,13 @@
 #include <aes.h>
 #include <sha.h>
 
+const RAND_METHOD *hc_rand_unix_method = NULL;
+
 void
 RAND_seed(const void *indata, size_t size)
 {
 #if 1 || defined(AFS_AIX_ENV) || defined(AFS_DFBSD_ENV) || defined(AFS_HPUX_ENV) || defined(AFS_SGI_ENV) || defined(UKERNEL)
-    RAND_METHOD *m = RAND_fortuna_method();
+    const RAND_METHOD *m = RAND_fortuna_method();
     m->seed(indata, size);
 #else
     /* Do nothing; we use the kernel's RNG */
@@ -25,7 +27,7 @@ RAND_bytes(void *outdata, size_t size)
     if (size == 0)
 	return 0;
 #if 1 || defined(AFS_AIX_ENV) || defined(AFS_DFBSD_ENV) || defined(AFS_HPUX_ENV) || defined(AFS_SGI_ENV) || defined(UKERNEL)
-    RAND_METHOD *m = RAND_fortuna_method();
+    const RAND_METHOD *m = RAND_fortuna_method();
     return m->bytes(outdata, size);
 #else
     if (osi_readRandom(outdata, size))
