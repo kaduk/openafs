1e4a19c2adbf19c3942220ab1dd15f10b99b5e49
Ben Kaduk <kaduk@mit.edu>
2013-11-26 16:17:00 -0500

    local patch
    This machine is in something of a heisen-state.

--- src/afs/afs_pioctl.c
+++ src/afs/afs_pioctl.c
@@ -820,7 +820,7 @@ afs_xioctl(afs_proc_t *p, const struct ioctl_args *uap, register_t *retval)
 #if defined(AFS_NBSD50_ENV)
     if ((fd = fd_getfile(SCARG(uap, fd))) == NULL)
 	return (EBADF);
-#elif defined(AFS_FBSD100_ENV)
+#elif 0
     if ((uap->fd >= fdp->fd_nfiles)
 	|| ((fd = fdp->fd_ofiles[uap->fd].fde_file) == NULL))
 	return EBADF;
