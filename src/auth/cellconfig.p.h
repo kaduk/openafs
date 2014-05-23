e9a9f61c5d1d8526b81f72382e6df7a2a28042e6
Benjamin Kaduk <kaduk@mit.edu>
2014-05-23 17:11:30 -0400

    Add afsconf getkey variants for RXGKFS
    That is, getkey routines for use by fileservers.
    
    Fileservers will keep their token-encrypting keys in KeyFileExt,
    using a new key type afsconf_rxgk_fs.  Such keys are only used
    if no keys of type afsconf_rxgk are present (that is, the cell-wide
    key takes precedence, both at token-decyryption time and at
    vlserver-registration time).
    
    Also fix some whitespace nits where the KeyFileExt key types
    are defined.
    
    Change-Id: Ife585cc976ffe1b1f86d9f285f3be9e599c75c29

--- src/auth/cellconfig.p.h
+++ src/auth/cellconfig.p.h
@@ -151,10 +151,14 @@ extern afs_int32 afsconf_GetLatestKey(struct afsconf_dir *adir,
 extern afs_int32 afsconf_GetLatestRXGKKey(struct afsconf_dir *adir,
 					  afs_int32 *kvno, afs_int32 *enctype,
 					  rxgk_key *key);
+int afsconf_GetLatestRXGKFSKey(struct afsconf_dir *rock, afs_int32 *kvno,
+			       afs_int32 *enctype, rxgk_key *key);
 extern int afsconf_GetKey(void *rock, int avno,
 			  struct ktc_encryptionKey *akey);
 extern int afsconf_GetRXGKKey(void *rock, afs_int32 *vno, afs_int32 *enctype,
 			      rxgk_key *key);
+int afsconf_GetRXGKFSKey(void *rock, afs_int32 *kvno,
+			 afs_int32 *enctype, rxgk_key *key);
 extern int afsconf_AddKey(struct afsconf_dir *adir, afs_int32 akvno,
 			  char akey[8], afs_int32 overwrite);
 extern int afsconf_DeleteKey(struct afsconf_dir *adir, afs_int32 akvno);
@@ -167,8 +171,9 @@ struct afsconf_typedKeyList {
 
 typedef enum {
     afsconf_rxkad = 0,
-    afsconf_rxgk  =1,
-    afsconf_rxkad_krb5  =2
+    afsconf_rxgk = 1,
+    afsconf_rxkad_krb5 = 2,
+    afsconf_rxgk_fs = 3
 } afsconf_keyType;
 
 extern struct afsconf_typedKey *
